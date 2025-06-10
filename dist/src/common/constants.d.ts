cd44f9dc0b480002a3dd4d414b52af26d00d0965
Artavazd Barseghyan <artavazd1@gmail.com>
2025-06-10 22:54:32 +0200

    remove dist

--- dist/src/common/constants.d.ts
+++
@@ -1,3 +0,0 @@
-export declare enum AuditLogKafkaTopic {
-    CreateLog = "auditlog.createLog"
-}
--- dist/src/common/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AuditLogKafkaTopic = void 0;
-var AuditLogKafkaTopic;
-(function (AuditLogKafkaTopic) {
-    AuditLogKafkaTopic["CreateLog"] = "auditlog.createLog";
-})(AuditLogKafkaTopic || (exports.AuditLogKafkaTopic = AuditLogKafkaTopic = {}));
